4912ecd14351f3d98c45a447a4603d428d0ad91c
vishalbakshi <vdbakshi@gmail.com>
2019-01-06 09:57:16 -0800

    add defaultProblem to server.js and update practiceProblems.json structure

--- src/server.js
+++ src/server.js
@@ -1,20 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 const dotenv = require("dotenv");
 const express = require("express");
 
 dotenv.config();
 const app = express();
 
+// bring in practiceProblems.json
+const readJson = function(path, callback) {
+  fs.readFile(require.resolve(path), function(err, data) {
+    if (err) {
+      return console.error("Error reading json file: ", err);
+    }
+    return callback(JSON.parse(data));
+  });
+};
+
+let defaultProblem = {
+  topic: "Fundamentals",
+  problemStatement: "What is the value of x given the following?",
+  knownVariables: { y: 2 },
+  unknownVariable: "x",
+  relevantFormulas: "y = x - 2",
+  answer: "x = 4"
+};
+let practiceProblems;
+readJson("./practiceProblems.json", function(data) {
+  practiceProblems = data;
+});
+// app methods
 app.set("view engine", "pug");
 
 app.route("/").get(function(req, res) {
-  res.render("index", {
-    topic: "Shear Stress",
-    problemStatement: "Problem Statement text",
-    knownVariables: ["known variable 1", "known variable 2"],
-    unknownVariable: "unknown variable",
-    answer: [5.6, "lbs"]
-  });
+  // Create default problem
+
+  res.render("index", defaultProblem);
 });
 
 app.listen(8080, function() {
