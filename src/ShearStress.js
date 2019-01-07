f48bf5037c45242c7c79e50d9bf648fc74bab861
vishalbakshi <vdbakshi@gmail.com>
2019-01-06 18:27:55 -0800

    add getProblem() to ShearStress
    Added a `getProblem` function to ShearStress which will return a practiceProblem object based on the given unit system string

--- src/ShearStress.js
+++ src/ShearStress.js
@@ -1,3 +1,32 @@
+exports.getProblem = function getProblem(unitSystem) {
+  let eta, delta_v, delta_y;
+  if (unitSystem == "metric") {
+    eta = [0.001 + Math.random(), "Pa*s"];
+    delta_v = [Math.floor(Math.random() * 5 + 1), "m/s"];
+    delta_y = [Math.floor(Math.random() * 5 + 1), "m"];
+  }
+
+  if (unitSystem == "imperial") {
+    eta = [0.00003 + Math.random(), "lb*s/ft^2"];
+    delta_v = [Math.floor(Math.random() * 15 + 1), "ft/s"];
+    delta_y = [Math.floor(Math.random() * 15 + 1), "ft"];
+  }
+
+  let practiceProblem = {
+    topic: "Shear Stress",
+    problemStatement:
+      "What is the shear stress inside of a fluid with the following properties?",
+    knownVariables: {
+      eta: eta,
+      delta_v: delta_v,
+      delta_y: delta_y
+    },
+    unknownVariable: "tau",
+    relevantFormulas: "tau = eta * delta_v / delta_y"
+  };
+  return practiceProblem;
+};
+
 exports.ShearStress = function ShearStress(knownVariables) {
   // let variables = ["eta", "delta_v", "delta_y", "tau"];
   // Extract variables from input and convert to Double
@@ -7,5 +36,10 @@ exports.ShearStress = function ShearStress(knownVariables) {
 
   // Calculate tau
   let tau = (eta * delta_v) / delta_y;
-  return { tau: [tau, "unit"] };
+
+  // get correct units of tau
+  if (knownVariables.delta_y[1] == "m") tauUnit = "Pa";
+  if (knownVariables.delta_y[1] == "ft") tauUnit = "lb/ft^2";
+
+  return { tau: [tau, tauUnit] };
 };
--- src/ShearStressTests.js
+++ src/ShearStressTests.js
@@ -1,5 +1,11 @@
 const test = require("tape");
 const ShearStress = require("./ShearStress").ShearStress;
+// input argument for ShearStress function
+let knownVariables = {
+  eta: [1, "unit"],
+  delta_v: [1, "unit"],
+  delta_y: [1, "unit"]
+};
 
 // test if ShearStress is a function
 test("ShearStress is a function", function(t) {
@@ -9,22 +15,45 @@ test("ShearStress is a function", function(t) {
 
 // test if ShearStress returns an array
 test("ShearStress returns an object", function(t) {
-  let knownVariables = {
-    eta: [1, "unit"],
-    delta_v: [1, "unit"],
-    delta_y: [1, "unit"]
-  };
   t.equal(typeof ShearStress(knownVariables), "object");
   t.end();
 });
 
 // test if ShearStress return object has a key 'tau'
 test("ShearStress return object has `tau` key", function(t) {
-  let knownVariables = {
-    eta: [1, "unit"],
-    delta_v: [1, "unit"],
-    delta_y: [1, "unit"]
-  };
   t.looseEqual(Object.keys(ShearStress(knownVariables)), ["tau"]);
+  t.end();
+});
+
+// test if ShearStress return object['tau'] is an array
+test("ShearStress return object[`tau`] is an array", function(t) {
+  t.looseEqual(Array.isArray(ShearStress(knownVariables)["tau"]), true);
+  t.end();
+});
+
+// test if ShearStress calculates `tau` as expected value
+test("ShearStress returns correct value of `tau`", function(t) {
+  let knownVariables = {
+    eta: [Math.floor(Math.random() * 10 + 1), "unit"],
+    delta_v: [Math.floor(Math.random() * 10 + 1), "unit"],
+    delta_y: [Math.floor(Math.random() * 10 + 1), "unit"]
+  };
+
+  let tau =
+    (knownVariables.eta[0] * knownVariables.delta_v[0]) /
+    knownVariables.delta_y[0];
+
+  t.equal(ShearStress(knownVariables)["tau"][0], tau);
+  t.end();
+});
+
+test("ShearStress returns correct unit of `tau`", function(t) {
+  let knownVariables = {
+    eta: [Math.floor(Math.random() * 10 + 1), "unit"],
+    delta_v: [Math.floor(Math.random() * 10 + 1), "unit"],
+    delta_y: [Math.floor(Math.random() * 10 + 1), "unit"]
+  };
+
+  t.equal(ShearStress(knownVariables)["tau"][1], "unit");
   t.end();
 });
