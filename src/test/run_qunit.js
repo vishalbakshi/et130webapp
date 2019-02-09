b74232e064c3c3fc859c58ed6f681a2e2cf10c3e
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-09 13:50:56 -0800

    update run_qunit.js

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -80,6 +80,8 @@ page.open(system.args[1], function(status) {
       },
       function() {
         var failedNum = page.evaluate(function() {
+          let failedTests = document.getELementsByClassName("fail");
+          console.log(failedTests[0]);
           var el = document.getElementById("qunit-testresult");
           console.log(el.innerText);
           try {
