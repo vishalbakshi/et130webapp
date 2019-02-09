925356c1e606fc60062e0daab373745b689f00cc
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-08 19:24:23 -0800

    Update run_qunit.js

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -1,21 +1,5 @@
 "use strict";
 
-/*
-const phantom = require("phantomjs-prebuilt");
-const page = require("webpage").create();
-
-page.open("http://localhost:8080/test", function(status) {
-  if (status != "success") {
-    console.log("Unable to access network");
-    phantom.exit(1);
-  } else {
-    // do the checks for tests
-    let title = page.evaluate(function() {
-      return document.title;
-    });
-  }
-});
-*/
 var page = require("webpage").create();
 
 page.open("http://localhost:8080/test", function() {
