bbc693c19f56c151d85ad53d6e19c13d60598efd
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-09 09:47:15 -0800

    Update run_qunit.js
    simplify the phantomjs test

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -3,11 +3,5 @@
 const page = require("webpage").create();
 const port = process.env.PORT || 8080;
 const url = "http://localhost:" + port + "/test";
-console.log(url);
-page.open(url, function() {
-  setTimeout(function() {
-    var passes = page.evaluate(function() {
-      phantom.exit(0);
-    });
-  }, 100);
-});
+
+phantom.exit(0);
