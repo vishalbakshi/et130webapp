658a273d0f93ad867c83e917730f12b13c221d43
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-09 09:38:00 -0800

    Update run_qunit.js
    add exit code 0 to phantom.exit() inside of page.evaluate function

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -7,11 +7,7 @@ const url = "http://localhost:" + port + "/test";
 page.open(url, function() {
   setTimeout(function() {
     var passes = page.evaluate(function() {
-      return [document.getElementsByClassName("pass").length, document.getElementsByClassName("fail").length;
+      phantom.exit(0);
     });
-
-    console.log(passes.length);
-
-    phantom.exit();
-  }, 5000);
+  }, 100);
 });
