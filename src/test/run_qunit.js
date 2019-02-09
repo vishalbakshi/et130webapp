bbb131a95c7ab64b0c62e9133ef5444bf04cee53
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-09 13:26:48 -0800

    update run_qunit.js

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -90,7 +90,8 @@ page.open(system.args[1], function(status) {
           return 10000;
         });
         console.log(failedNum);
-        console.log(document.getElementsByClassName("fail"));
+
+        console.log(document.getElementsByClassName("fail")[0]);
         phantom.exit(parseInt(failedNum, 10) > 0 ? 1 : 0);
       }
     );
