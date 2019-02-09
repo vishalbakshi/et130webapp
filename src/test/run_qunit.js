e81328a91374802608e988fff1a9529b5341b4b2
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-09 09:43:32 -0800

    Update run_qunit.js
    add console.log to see what's going on

--- src/test/run_qunit.js
+++ src/test/run_qunit.js
@@ -3,7 +3,7 @@
 const page = require("webpage").create();
 const port = process.env.PORT || 8080;
 const url = "http://localhost:" + port + "/test";
-
+console.log(url);
 page.open(url, function() {
   setTimeout(function() {
     var passes = page.evaluate(function() {
