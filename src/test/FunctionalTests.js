01e30384710928a2dcfaf18772d386e5b80bfaa6
Vishal Bakshi <vdbakshi@gmail.com>
2019-02-15 20:33:28 -0800

    update travis yml

--- src/test/FunctionalTests.js
+++ src/test/FunctionalTests.js
@@ -7,7 +7,7 @@ const selenium = require("selenium-webdriver");
 const server = require("../server.js")
 chai.use(chaiHttp);
 
-const port = process.env.PORT || 8080;
+const port = 8081;
 
 test("GET response contains correct page headings", function(done) {
   chai
